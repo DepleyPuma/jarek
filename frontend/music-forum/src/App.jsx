d1d1df1df5c61bfad5b3978322d8f663250a6a26
DepleyPuma <szymon.szarek1@wp.pl>
2024-01-12 20:42:53 +0100

    created new const varaiables "links" adedd new props for Nav and Header

--- frontend/music-forum/src/App.jsx
+++ frontend/music-forum/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import { Header } from './components/Header/Header';
 import { Nav } from './components/Nav/Nav';
+import { AboutUs } from './components/AboutUs/AboutUs';
+
+const links = {
+	home: '#',
+	aboutUs: '#about-us',
+	events: '#events',
+};
 
 function App() {
 	const [navIsOpen, setNavIsOpen] = useState(false);
 
 	return (
 		<>
-			<Nav navIsOpen={navIsOpen} onClick={() => setNavIsOpen(prevState => !prevState)} />
-			<Header navIsOpen={navIsOpen} />
-			<div className='min-h-[200svh] bg-violet'></div>
+			<Nav links={links} navIsOpen={navIsOpen} onClick={() => setNavIsOpen(prevState => !prevState)} />
+			<Header links={links} navIsOpen={navIsOpen} />
+			<main>
+				<AboutUs />
+			</main>
+			<div className='min-h-[200svh] bg-gradient-to-r from-violet to-[#120a27]'></div>
 		</>
 	);
 }
