9bff16b6bf07572d453f97c9144ed29053da5c1c
DepleyPuma <szymon.szarek1@wp.pl>
2024-01-12 20:46:18 +0100

    this is list for section about-us

---
+++ frontend/music-forum/src/components/OurOffers/OurOffers.jsx
@@ -0,0 +1,38 @@
+export function OurOffers() {
+	return (
+		<>
+			<p className='font-bold text-3xl text-center'>Co oferujemy: </p>
+			<ul className='marker:text-purple-600 list-disc text-lg space-y-8 md:space-y-28 md:text-2xl md:leading-10'>
+				<li>
+					<p className='leading-6 md:leading-10'>
+						<span className='ml-2'>Wejdź</span> do naszego muzycznego świata, gdzie czeka na Ciebie przestrzeń do
+						wymiany myśli, odkrywania nowych artystów i dzielenia się ulubionymi utworami. Nasza społeczność to miejsce,
+						gdzie zawsze znajdziesz kogoś gotowego do podzielenia się swoją pasją muzyczną.
+					</p>
+				</li>
+				<li>
+					<p className='leading-6 md:leading-10'>
+						<span className='ml-2'>Zakreśl</span> daty w kalendarzu! Dzięki zakładce "Wydarzenia" każdy użytkownik ma
+						szansę podzielić się informacjami o najbliższych koncertach, festiwalach czy innych muzycznych wydarzeniach.
+						Bądź na bieżąco z tym, co dzieje się w świecie muzyki i wspólnie z nami uczestnicz w niezapomnianych
+						przeżyciach.
+					</p>
+				</li>
+				<li>
+					<p className='leading-6 md:leading-10'>
+						<span className='ml-2'>Ty również</span> masz moc tworzenia wyjątkowych chwil muzycznych! Dzięki funkcji
+						dodawania własnych wydarzeń możesz informować społeczność o koncertach, jam sessions czy nawet prywatnych
+						występach. Pozwól innym dołączyć do Twojego muzycznego świata!
+					</p>
+				</li>
+				<li>
+					<p className='leading-6 md:leading-10'>
+						<span className='ml-2'>Nasze</span> forum to idealne miejsce do znalezienia osób, które podzielają Twoje
+						muzyczne zainteresowania. Poznawaj nowych ludzi, wymieniaj się doświadczeniami koncertowymi i twórz muzyczne
+						przyjaźnie.
+					</p>
+				</li>
+			</ul>
+		</>
+	);
+}
