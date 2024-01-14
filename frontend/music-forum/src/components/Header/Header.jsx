79e62a76b8c28216b28a260bedf9cfa297e89c71
DepleyPuma <szymon.szarek1@wp.pl>
2024-01-14 01:25:24 +0100

    fixed problem whith paddin on button

--- frontend/music-forum/src/components/Header/Header.jsx
+++ frontend/music-forum/src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ export function Header({ navIsOpen, links }) {
 	return (
 		<>
 			<header className='relative'>
-				<video className='w-full h-screen object-cover' autoPlay muted loop src={video}></video>
+				<video className='w-full h-screen object-cover' muted loop src={video}></video>
 				{!navIsOpen && (
 					<div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-center text-white'>
 						<h1 className='my-16 text-4xl uppercase font-bold md:text-6xl lg:text-8xl'>Baw się z nami</h1>
-						<button className='px-8 py-4 border border-zinc-300 rounded-full animate-bounce'>
-							<a href={links.aboutUs}>Sprawdź</a>
+						<button className='py-4 border border-zinc-300 rounded-full animate-bounce'>
+							<a href={links.aboutUs} className='px-16'>
+								Sprawdź
+							</a>
 						</button>
 					</div>
 				)}
