3cd4a9188c1eb953893c93d8fca1fcd94e0dc7df
DepleyPuma <szymon.szarek1@wp.pl>
2024-01-14 13:25:15 +0100

    commit

--- frontend/music-forum/src/components/Header/Header.jsx
+++ frontend/music-forum/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export function Header({ navIsOpen, links }) {
 	return (
 		<>
 			<header className='relative'>
-				<video className='w-full h-screen object-cover' muted loop src={video}></video>
+				<video className='w-full h-screen object-cover' autoPlay muted loop src={video}></video>
 				{!navIsOpen && (
 					<div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-center text-white'>
 						<h1 className='my-16 text-4xl uppercase font-bold md:text-6xl lg:text-8xl'>Baw się z nami</h1>
