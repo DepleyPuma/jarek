8f3605b3cd456e3308f05f6ef65837cfe6cd2b47
DepleyPuma <szymon.szarek1@wp.pl>
2024-01-15 14:26:26 +0100

    change width line under h2 tag

--- frontend/music-forum/src/components/SectionHeader/SectionHeader.jsx
+++ frontend/music-forum/src/components/SectionHeader/SectionHeader.jsx
@@ -1,8 +1,8 @@
 export function SectionHeader({ children }) {
 	return (
-		<h2 className='text-6xl font-extralight uppercase text-center text-white'>
+		<h2 className='text-4xl font-extralight uppercase text-center text-white md:text-6xl'>
 			{children}
-			<hr className='mx-auto mt-4 w-[250px] border-2' />
+			<hr className='mx-auto mt-4 w-[150px] border-2' />
 		</h2>
 	);
 }
